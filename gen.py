ecbb313282c00b6d16e5801de1bdeb2eea4a8836
Kotaz <semechkagent@gmail.com>
2021-12-31 13:33:06 +0300

    Запуск black

--- gen.py
+++ gen.py
@@ -3,23 +3,27 @@ import re
 import glob
 import os
 
+
 class Object(object):
     pass
 
+
 environment = Object()
 
+
 def process(page):
-    temp_start = '<python>'
-    temp_end = '</python>'
+    temp_start = "<python>"
+    temp_end = "</python>"
     regex = f"{temp_start}.+?{temp_end}"
     search_result = re.findall(regex, page, re.DOTALL)[::-1]
     count = len(search_result)
+
     def get_replace(raw):
         last = raw.pop(-1)
-        fname = f'python_script_{count-len(raw)}'
-        code = f"def {fname}():\n" + last[len(temp_start):-len(temp_end)]
+        fname = f"python_script_{count-len(raw)}"
+        code = f"def {fname}():\n" + last[len(temp_start) : -len(temp_end)]
         exec(code, globals(), locals())
-        results = ''.join(map(str, locals()[fname]()))
+        results = "".join(map(str, locals()[fname]()))
         return results
 
     while True:
@@ -29,21 +33,21 @@ def process(page):
             break
     return page
 
+
 # html = open("input.html").read()
 
-patterns = ['/src/*.html', '/src/**/*.html']
+patterns = ["/src/*.html", "/src/**/*.html"]
 for pattern in patterns:
-    all_path = glob.glob(os.getcwd()+pattern)
+    all_path = glob.glob(os.getcwd() + pattern)
     for path in all_path:
         content = open(path).read()
         result = process(content)
-        respath = path.replace('/src', '')
+        respath = path.replace("/src", "")
         if not os.path.exists(os.path.dirname(respath)):
             try:
                 os.makedirs(os.path.dirname(respath))
-            except OSError as exc: # Guard against race condition
+            except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
         with open(respath, "w", encoding="utf-8") as f:
             f.write(result)
-
